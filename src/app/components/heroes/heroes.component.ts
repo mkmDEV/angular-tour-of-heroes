50f18df788574a0d361c13f247e7921a4daa570c
mkmdev <mkmdev18@gmail.com>
2019-08-28 11:07:54 +0200

    moved models to separate directory

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import { Hero } from '../../hero';
-import { HEROES } from '../../mock-heroes';
+import { Hero } from '../../models/hero';
+import { HEROES } from '../../models/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
