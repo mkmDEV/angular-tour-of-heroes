f588d08198ec2e2a855ded5a899e8ff637610977
mkmdev <mkmdev18@gmail.com>
2019-08-28 13:02:50 +0200

    DI heroes and add observe data

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -8,10 +8,17 @@ import {HeroService} from '../../services/hero.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private heroService: HeroService) {
   }
 
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
 }
