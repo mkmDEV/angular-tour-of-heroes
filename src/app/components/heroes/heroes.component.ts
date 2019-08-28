ae0504abe1846ed2d3ee1c49bda6378f400c2335
mkmdev <mkmdev18@gmail.com>
2019-08-28 11:38:01 +0200

    Replaced importing mock heroes with heroService

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import { Hero } from '../../models/hero';
-import { HEROES } from '../../models/mock-heroes';
+import {Hero} from '../../models/hero';
+import {HeroService} from '../../services/hero.service';
 
 @Component({
   selector: 'app-heroes',
