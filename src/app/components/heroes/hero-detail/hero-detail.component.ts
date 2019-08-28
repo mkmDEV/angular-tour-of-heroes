2e8379d5279858620969ce5e71c9cfee8d88d9a5
mkmdev <mkmdev18@gmail.com>
2019-08-28 13:49:54 +0200

    get hero details by clicking on a hero in dashboard, and ability to get back

--- src/app/components/heroes/hero-detail/hero-detail.component.ts
+++ src/app/components/heroes/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
 import {HeroService} from '../../../services/hero.service';
@@ -10,15 +10,26 @@ import {Hero} from '../../../models/hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location,
-  ) {}
-
-  ngOnInit() {
+  ) {
   }
 
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
