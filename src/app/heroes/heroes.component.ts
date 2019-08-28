cf95eb7b2203049bfff7fdcd3db0bef61fa3b85f
mkmdev <mkmdev18@gmail.com>
2019-08-28 10:47:42 +0200

    split heros and selected component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -15,6 +15,7 @@ export class HeroesComponent implements OnInit {
   }
 
   selectedHero: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
